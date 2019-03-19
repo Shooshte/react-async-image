4be67a1209f7fee886e9b31d6ae519e1b290a62e
shooshte <miha.sustersic.work@gmail.com>
2019-03-19 17:31:42 +0100

    initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,22 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import Example from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-async-image Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <Example
+          width="1000px"
+          height="1000px"
+          placeholderImage="http://lorempixel.com/20/20/"
+          actualImage="http://lorempixel.com/1900/1900/"
+          placeholderColor="#000"
+        />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,94 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import "./index.css";
 
-export default class extends Component {
+export default class Image extends Component {
+  state = {
+    hasPlaceholderError: false,
+    hasImageError: false,
+    isLoadingPlaceholder: true,
+    isLoading: true
+  };
+
+  onLoadPlaceholder = () => {
+    this.setState({
+      isLoadingPlaceholder: false
+    });
+  };
+
+  onLoadImage = () => {
+    this.setState({
+      isLoading: false
+    });
+  };
+
+  onPlaceholderError = () => {
+    this.setState({
+      hasPlaceholderError: true
+    });
+  };
+
+  onImageError = () => {
+    this.setState({
+      hasError: true
+    });
+  };
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const {
+      isLoadingPlaceholder,
+      isLoading,
+      hasImageError,
+      hasPlaceholderError
+    } = this.state;
+    const {
+      width,
+      height,
+      placeholderImage,
+      actualImage,
+      placeholderColor
+    } = this.props;
+
+    return (
+      <div
+        style={{
+          width: width,
+          height: height,
+          backgroundColor: placeholderColor
+        }}
+      >
+        {!hasPlaceholderError && (
+          <img
+            className={
+              isLoadingPlaceholder
+                ? "transparent"
+                : isLoading
+                ? "fade-in placeholder"
+                : "transparent"
+            }
+            style={{
+              width: width,
+              height: height,
+              position: "absolute"
+            }}
+            src={placeholderImage}
+            onLoad={this.onLoadPlaceholder}
+            onError={this.onPlaceholderError}
+          />
+        )}
+        {!hasImageError && (
+          <img
+            className={isLoading ? "transparent" : "fade-in"}
+            style={{
+              width: width,
+              height: height,
+              position: "absolute"
+            }}
+            src={actualImage}
+            onLoad={this.onLoadImage}
+            onError={this.onImageError}
+          />
+        )}
+      </div>
+    );
   }
 }
